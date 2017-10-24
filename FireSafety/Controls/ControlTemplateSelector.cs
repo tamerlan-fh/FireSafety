eb2d89da8f9f035357a5680f97339b7cc7bf190f
Tamerlan <Tamerlan@TAMERLAN->
2017-10-24 07:21:22 +0500

    правки по внешнему виду

--- FireSafety/Controls/ControlTemplateSelector.cs
+++ FireSafety/Controls/ControlTemplateSelector.cs
@@ -7,23 +7,23 @@ namespace FireSafety.Controls
     public class ControlTemplateSelector : DataTemplateSelector
     {
         public DataTemplate ControlTemplateОбъект { get; set; }
-        public DataTemplate ControlTemplateДверь { get; set; }
-        public DataTemplate ControlTemplateЭтаж { get; set; }
-        public DataTemplate ControlTemplateСтарт { get; set; }
-        public DataTemplate ControlTemplateПуть { get; set; }
-        public DataTemplate ControlTemplateУчастокПути { get; set; }
-        public DataTemplate ControlTemplateЗдание { get; set; }
-        public DataTemplate ControlTemplateУзелЛестницы { get; set; }
+        public DataTemplate ControlTemplateEntryNode { get; set; }
+        public DataTemplate ControlTemplateFloor { get; set; }
+        public DataTemplate ControlTemplateStartNode { get; set; }
+        public DataTemplate ControlTemplateRoute { get; set; }
+        public DataTemplate ControlTemplateRoadSection { get; set; }
+        public DataTemplate ControlTemplateBuilding { get; set; }
+        public DataTemplate ControlTemplateStairsNode { get; set; }
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
             if (item == null) return null;
-            if (item is EntryNode) return ControlTemplateДверь;
-            if (item is Floor) return ControlTemplateЭтаж;
-            if (item is Building) return ControlTemplateЗдание;
-            if (item is StartNode) return ControlTemplateСтарт;
-            if (item is Route) return ControlTemplateПуть;
-            if (item is RoadSection) return ControlTemplateУчастокПути;
-            if (item is StairsNode) return ControlTemplateУзелЛестницы;
+            if (item is EntryNode) return ControlTemplateEntryNode;
+            if (item is Floor) return ControlTemplateFloor;
+            if (item is Building) return ControlTemplateBuilding;
+            if (item is StartNode) return ControlTemplateStartNode;
+            if (item is Route) return ControlTemplateRoute;
+            if (item is RoadSection) return ControlTemplateRoadSection;
+            if (item is StairsNode) return ControlTemplateStairsNode;
 
             return ControlTemplateОбъект;
         }
